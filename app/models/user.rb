1dd69b2d91b0d13078ebec9e565ed7ca37cc88a1
Marburg1234 <172342315+Marburg1234@users.noreply.github.com>
2024-08-11 06:23:53 +0000

    Register finishi-1-7

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # deviseですでに定義されているメソッド⇒サインインしたらaboutページへ遷移する
+  def after_sign_in_path_for(resource)
+    about_path
+  end
+
+  def after_sign_out_path_for(resource)
+    about_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+
+end
