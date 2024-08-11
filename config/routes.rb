f0fd90d8630f20b6526023e9bf6ff09b4f909c3d
Marburg1234 <172342315+Marburg1234@users.noreply.github.com>
2024-08-11 13:02:56 +0000

    Finish -17

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
 
-
-  get 'users/edit'
-  get 'users/show'
   devise_for :users
   root to: 'homes#top'
   get 'homes/about' => 'homes#about', as: 'about'
 
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
 
+  resources :users, only: [:edit, :show, :update]
 
 end
